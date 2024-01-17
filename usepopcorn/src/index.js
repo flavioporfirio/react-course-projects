111f8d6566300d9f14b0b0e959d68e18c78de698
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-01-17 16:17:23 -0300

    feat: reusable star rating component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
+import StarRating from "./StarRating";
+import "./index.css";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+    <Test /> */}
     <App />
   </React.StrictMode>
 );
