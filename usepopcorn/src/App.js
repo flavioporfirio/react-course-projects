ede9ad0e5f0cbeefffa1b16fbf57fd0c1b9714ab
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-01-29 18:04:43 -0300

    feat: MovieDetails component structure, loading and style

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -89,7 +90,7 @@ export default function App() {
 
   useEffect(
     function () {
-      async function fetchMovies() {
+      async function fetchMovie() {
         try {
           setIsLoading(true);
           setError("");
@@ -115,31 +116,9 @@ export default function App() {
         setError("");
         return;
       }
-      fetchMovies();
+      fetchMovie();
     },
     [query]
-  );
-
-  useEffect(
-    function () {
-      async function SelectedMovie() {
-        try {
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
-          );
-          const data = await res.json();
-          console.log(data);
-
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          if (!data.Response) throw new Error(data.Error);
-        } catch (err) {
-          console.error(err.message);
-        }
-      }
-      SelectedMovie();
-    },
-    [selectedId]
   );
 
   return (
@@ -269,12 +248,80 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function SelectedMovie() {
+        setIsLoading(true);
+        try {
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          );
+          const data = await res.json();
+          setMovie(data);
+          setIsLoading(false);
+
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          if (!data.Response) throw new Error(data.Error);
+        } catch (err) {
+          console.error(err.message);
+        }
+      }
+      SelectedMovie();
+    },
+    [selectedId]
+  );
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie}`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>{released}</p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
