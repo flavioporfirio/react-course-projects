42e1de3c6a71902864e1bffda30ec75ad95de157
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-03-02 16:54:03 -0300

    feat: add pages, components, and PageNav component to navigate between them

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,3 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 export default function App() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+/*
+   <Route path="*" element={<PageNotFound />} />
+
+   - caso nenhuma rota for selecionada, esta rota acima é chamada
+*/
